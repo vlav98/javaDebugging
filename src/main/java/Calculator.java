4b6d66138c16204b20f84af095fe37f190845232
Valentine Lav <valentinelav98@gmail.com>
2023-12-17 19:13:03 +0100

    Added double function

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -12,6 +12,14 @@ public class Calculator {
         return a * b;
     }
 
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+
     public void longCalculation() {
         try {
             Thread.sleep(500);
